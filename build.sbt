9f82ac91fceee71b084e155e304735a98159a73e
guoyang2011 <guoyang2011@gmail.com>
2015-07-24 16:08:29 +0800

    init kafka scala template

---
+++ build.sbt
@@ -0,0 +1,10 @@
+oneJarSettings
+
+name := "kafkaStart"
+
+version := "1.0"
+
+scalaVersion := "2.10.4"
+
+
+libraryDependencies += "org.apache.kafka" % "kafka_2.10" % "0.8.2.0"
---
+++ src/main/scala-2.10/cn/changhong/kafka/Start.scala
@@ -0,0 +1,85 @@
+package cn.changhong.kafka
+
+import java.io.FileInputStream
+import java.util.Properties
+import java.util.concurrent.Executors
+
+import kafka.consumer.{Consumer, ConsumerConfig}
+import kafka.producer._
+import kafka.utils.{VerifiableProperties, Logging}
+
+
+/**
+ * Created by yangguo on 15-7-23.
+ */
+class CustomizePartitioner(props: VerifiableProperties = null) extends Partitioner{
+  override def partition(key: Any, numPartitions: Int): Int = new DefaultPartitioner(props).partition(key,numPartitions)
+}
+object Start {
+  private def createConfig(path:String)={
+    val _props=new Properties()
+    val inputStream=new FileInputStream(path)
+    _props.load(inputStream)
+    _props
+  }
+
+  object ConsumerExample extends Logging{
+    def apply(config:Properties)={
+      val consumerConfig=new ConsumerConfig(config)
+      val consumer=Consumer.create(consumerConfig)
+      val executor=Executors.newFixedThreadPool(4)
+      val topicCountMap=Map(topic->4)
+      val consumerMap=consumer.createMessageStreams(topicCountMap)
+      consumerMap.get(topic) match {
+        case Some(streams) =>
+          streams.foreach { stream =>
+            val busTask = new Runnable {
+              override def run(): Unit = {
+                val it = stream.iterator()
+                Iterator continually (it.hasNext()) takeWhile (_ == true) foreach { bool =>
+                  val msg = new String(it.next().message(), "utf8")
+                  println(s"groupId[${groupId}],${System.currentTimeMillis()},Thread ${Thread.currentThread().getId},msg=$msg")
+                }
+                println("isOver")
+              }
+            }
+            executor.submit(busTask)
+          }
+        case None=>throw new RuntimeException("No Valid Kafka Stream!")
+      }
+    }
+  }
+
+  object ProducerExample extends Logging{
+    def apply(config:Properties)={
+      val _config=new ProducerConfig(config)
+      val producer=new Producer[String,String](_config)
+      (1 to events).foreach{index=>
+        val runtime=System.currentTimeMillis()
+        val msg=s"msgId[$runtime],eventId[$index],msg[helloworld]"
+        val data=new KeyedMessage[String,String](topic,rnd.nextInt().toString,msg)
+        producer.send(data)
+      }
+    }
+  }
+
+  def main(args: Array[String]) {
+    require(args.length>=2,"Please Enter executor type[producer or consumer] and Kafka Config File Path")
+    def run(func: =>Unit)={
+      new Thread(new Runnable {
+        override def run(): Unit = func
+      }).start()
+    }
+    val executorType=args(0)
+    val propsFile=args(1)
+    val config=createConfig(propsFile)
+    if(executorType.equalsIgnoreCase("producer")){
+      run(ProducerExample(config))
+    }else if(executorType.equalsIgnoreCase("consumer")) {
+      run(ConsumerExample(config))
+    }else{
+      throw new RuntimeException("Not Find Valid Type,Please Enter Valid Type [producer or consumer]")
+    }
+  }
+
+}
